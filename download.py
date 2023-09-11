d66fc9f2d72dba5cf595c02b01c9b2b3a16b55b9
Stefal <stephane.peneau@wanadoo.fr>
2023-09-11 22:56:21 +0200

    WIP : using writer.py from panoramax project.

--- download.py
+++ download.py
@@ -5,6 +5,7 @@ import asyncio
 import argparse
 from datetime import datetime
 from lib.exif_write import ExifEdit
+import writer
 
 def parse_args(argv =None):
     parser = argparse.ArgumentParser()
@@ -26,19 +27,20 @@ def download(url, fn, metadata=None):
     f.write(r.content)
   write_exif(fn, metadata)
 
-def write_exif(filename, data):
+def write_exif(filename, img_metadata):
     '''
     Write exif metadata
     '''
     #{'thumb_original_url': 'https://scontent-cdg4-2.xx.fbcdn.net/m1/v/t6/An9Zy2SrH9vXJIF01QkBODyUbg7XSKfwL48UwHyvihSwvECGjVbG0vSw9uhxe2-Dq-k2eUcigb83buO6zo-7eVbykfp5aQIe1kgd-MJr66nU_H-o_mwBLZXgVbj5I_5WX-C9c6FxJruHkV962F228O0?ccb=10-5&oh=00_AfDOKD869DxL-4ZNCbVo8Rn29vsc0JyjMAU2ctx4aAFVMQ&oe=65256C25&_nc_sid=201bca',
     #  'captured_at': 1603459736644, 'geometry': {'type': 'Point', 'coordinates': [2.5174596904057, 48.777089857534]}, 'id': '485924785946693'}
-    lat = data['geometry']['coordinates'][1]
+    """ lat = data['geometry']['coordinates'][1]
     long = data['geometry']['coordinates'][0]
     altitude = data['altitude']
     bearing = data['compass_angle']
     timestamp=datetime.utcfromtimestamp(int(data['captured_at'])/1000)
-    metadata = metadata = ExifEdit(filename)
+    """
     
+    metadata = ExifEdit(filename)
     #metadata.read()
     
     try:
@@ -53,17 +55,17 @@ def write_exif(filename, data):
         #metadata["Exif.GPSInfo.GPSImgDirection"] = exiv_bearing
         #metadata["Exif.GPSInfo.GPSImgDirectionRef"] = "T"
         
-        metadata.add_lat_lon(lat, long)
-        metadata.add_altitude(altitude)
-        metadata.add_date_time_original(timestamp)
-        metadata.add_direction(bearing)
+        metadata.add_lat_lon(img_metadata.latitude, img_metadata.longitude)
+        metadata.add_altitude(img_metadata.altitude)
+        metadata.add_date_time_original(img_metadata.capture_time)
+        metadata.add_direction(img_metadata.direction)
         #if data['camera_type'] == 'spherical'
         metadata.write()
         print("Added geodata to: {0}".format(filename))
     except ValueError as e:
         print("Skipping {0}: {1}".format(filename, e))
     
-    if data['camera_type'] == 'spherical' :
+    if img_metadata.picture_type == "equirectangular" :
         print('Pano detected')
         import pyexiv2
         img = pyexiv2.Image(filename)
@@ -103,8 +105,8 @@ if __name__ == '__main__':
     urls = []
     print(img_num)
     print('getting urls')
-    for x in range(0, img_num):
-    #for x in range(0, 5):        
+    #for x in range(0, img_num):
+    for x in range(0, 5):        
         image_id = image_ids[x]['id']
         req_url = 'https://graph.mapillary.com/{}?fields=thumb_original_url,altitude,camera_type,captured_at,compass_angle,geometry,exif_orientation'.format(image_id)
         r = requests.get(req_url, headers=header)
@@ -115,5 +117,14 @@ if __name__ == '__main__':
 
     print('downloading.. this process will take a while. please wait')
     for i,url in enumerate(urls):
-        path = 'data/{}/{}.jpg'.format(sequence_id, datetime.utcfromtimestamp(int(url['captured_at'])/1000).strftime('%Y-%m-%d_%HH%Mmn%S.%f'))
-        download(url['thumb_original_url'],path, url)
+        date_time_image_filename = datetime.utcfromtimestamp(int(url['captured_at'])/1000).strftime('%Y-%m-%d_%HH%Mmn%S.%f')
+        path = 'data/{}/{}.jpg'.format(sequence_id, date_time_image_filename)
+        img_metadata = writer.PictureMetadata(
+                capture_time = datetime.utcfromtimestamp(int(url['captured_at'])/1000) ,
+                longitude = url['geometry']['coordinates'][0] ,
+                latitude = url['geometry']['coordinates'][1] ,
+                picture_type = "equirectangular" if url['camera_type'] == 'spherical' else None ,
+                direction = url['compass_angle'] ,
+                altitude = url['altitude']
+        )
+        download(url['thumb_original_url'],path, img_metadata)
